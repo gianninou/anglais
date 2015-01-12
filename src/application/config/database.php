8540c9982eba3f99422e04fda740598c547f0a0e
Valentin GIANNINI <valentin.giannini@telecomnancy.net>
2015-01-12 17:37:12 +0100

    update database.php and autoload session/database

--- src/application/config/database.php
+++ src/application/config/database.php
@@ -49,7 +49,7 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
+$db['default']['username'] = 'anglais';
 $db['default']['password'] = '';
 $db['default']['database'] = 'projet_anglais';
 $db['default']['dbdriver'] = 'mysql';
