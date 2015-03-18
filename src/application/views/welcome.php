5499e56ea47027983c72c04aed6841976855fdea
Valentin GIANNINI <valentin.giannini@telecomnancy.net>
2015-03-18 18:28:09 +0100

    add new banniere and welcome text

--- src/application/views/welcome.php
+++ src/application/views/welcome.php
@@ -1,3 +1,17 @@
 <div id="welcome_div">
-	Hello, ......
+	<p>Welcome to Fill In The Blank, our latest web application providing you awesome support to work your english vocabulary !</p>
+
+	<p>With Fill In The Blank, experience the future. See beyond the words, get the essence of English language and become the god of exams.
+	It's time to shine bright in front of your teachers, brace yourselves ... great marks are coming ...</p>
+
+	<p>What you can do with Fill In The Blank :</p>
+	<ul>
+	    <li>create vocabulary lists and test it.</li>
+	    <li>follow your progression.</li>
+	    <li>assess your knowledge by a focused training on the words you don't know.</li>
+	    <li>create groups of users and share lists with them !</li>
+	</ul>
+
+	<p>But first, be sure to register with your school domain e-mail (telecomnancy.eu) so that teachers can easily share with you their latest vocabulary lists ! </p>
+
 </div>
